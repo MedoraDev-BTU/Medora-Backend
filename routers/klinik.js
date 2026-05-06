84be7faab42baa830f8755fad2943203253647e5
Nermin <112072197+nermnbycn@users.noreply.github.com>
2026-05-06 21:55:44 +0300

    Delete routers directory

--- routers/klinik.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {klinikKayit}=require('../controllers/klinik')
-const router = express.Router();
-
-router.post("/kayit",klinikKayit); 
-
-
-
-module.exports = router; //disari aktarim yapmak icin
